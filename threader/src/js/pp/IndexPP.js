5b675f00d1b8f5731705de6da3942470c76abacc
gokarna14 <gokarna.adhikari14@gmail.com>
2022-02-05 11:41:17 +0545

    Completed the auto fill mechanism in PP

--- threader/src/js/pp/IndexPP.js
+++ threader/src/js/pp/IndexPP.js
@@ -11,6 +11,7 @@ const IndexPP = ()=>{
 
     const [questions, setQuestions] = useState(PP_questions);
     const [autoFill, setAutoFill] = useState(false);
+    const [autoFilledQuestion, setAutoFilledQuestion] = useState([]);
     const [options, setOptions] = useState({
         'Disagreed': 1,
         'Partially agreed': 2,
@@ -26,16 +27,44 @@ const IndexPP = ()=>{
     const selected = (question, option)=>{
         response[question] = options[option]
         setResponse({...response})
-        console.log(response)
+        autoFilledQuestion.pop(question)
+        setAutoFilledQuestion(autoFilledQuestion)
+        // console.log(response)
         remaining[0].value = 42 - Object.keys(response).length
         remaining[1].value = Object.keys(response).length
         setRemainingResponse([
             remaining[0],
             remaining[1]
         ])
-        console.log(remaining)
+        // console.log(remaining)
     }
- 
+    
+    const autoFillSelected=()=>{
+        setAutoFill(!autoFill)
+        if(!autoFill){
+            var questionKeys = Object.keys(questions)
+            for (var i in questionKeys){
+                var key = questionKeys[i]
+                var answers = response[key]
+                var answered = (answers !== undefined)
+                if (!answered){
+                    response[key] = parseInt(Math.floor(Math.random() * 4) + 1)
+                    autoFilledQuestion.push(key)
+                    setAutoFilledQuestion(autoFilledQuestion)
+                    // console.log(autoFilledQuestion)
+                }
+            }
+        }
+        else{
+            for (var i in autoFilledQuestion){
+                delete response[autoFilledQuestion[i]]
+            }
+            setResponse(response)
+            setAutoFilledQuestion([])
+        }
+        console.log(response)
+    }
+
 
     const questionsRender = Object.keys(questions).map(
         (i)=>{
@@ -43,13 +72,15 @@ const IndexPP = ()=>{
             return(
                 <form className="border border-danger" id="open">
                     {questions[i]}  {Object.keys(response).length > 0 && (response[i] === undefined ? '❌😅' : '✅😍')}
+                    {autoFilledQuestion.includes(i) ? '(AutoFilled)' : ""}
                     <div id="open">
                         {
                             Object.keys(options).map(
                                 (j)=>{
+                                    var aFill = autoFill &&  response[i] === undefined
                                     return(
                                         <>
-                                            {autoFill &&    <>
+                                            {aFill &&  <>
                                                 <div className="form-check">
                                                     <input className="form-check-input" 
                                                     type="radio" name={"flexRadioDefault"} 
@@ -58,18 +89,19 @@ const IndexPP = ()=>{
                                                     onChange={()=>{selected(i, j)}}
                                                     />
                                                     <label>
-                                                        {j}
+                                                        {j} 
                                                     </label>
                                                 </div>
                                             </>}
-                                            {!autoFill &&    <>
+                                            {!aFill &&    <>
                                                 <div className="form-check">
                                                     <input className="form-check-input" 
                                                     type="radio" name={"flexRadioDefault"} 
                                                     id="flexRadioDefault2" 
+                                                    checked = {response[i] == options[j]}
                                                     onChange={()=>{selected(i, j)}}/>
                                                     <label>
-                                                        {j}
+                                                        {j} 
                                                     </label>
                                                 </div>
                                             </>}
@@ -93,7 +125,7 @@ const IndexPP = ()=>{
                 <Progress showProgress={showProgress} setShowProgress={setShowProgress} remaining={remaining}></Progress>
             </div>
         }
-        <button className="btn btn-primary" onClick={()=>{setAutoFill(!autoFill)}}>Auto fill</button>
+        <button className="btn btn-primary" onClick={autoFillSelected}>{autoFill ? 'Cancel' : ''} Auto fill</button>
         <hr />
         <form className="niceCenter alignLeft" >
             <div className={(Object.keys(response).length > 0) && showProgress ? "niceCenterR" : ""}>
